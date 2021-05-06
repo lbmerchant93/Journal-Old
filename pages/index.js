15cffd8b6b9c37b7d8ac0a0cfd6fbc1672ce5dc8
Lucas Merchant <lbmerchant93@gmail.com>
2021-05-06 16:24:38 -0600

    Install Typescript

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>MiWi</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-    </div>
-  )
-}
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>MiWi</h1>
+      <p>Welcome to MiWi! Your personal Midwifery App!</p>
+    </div>
+  )
+}
