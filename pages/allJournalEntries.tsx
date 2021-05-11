ac353f1a889619b3ebd52797d995fcc9596c7c0c
Lucas Merchant <lbmerchant93@gmail.com>
2021-05-11 16:03:04 -0600

    Connect allJournalEntries page to query all journal entries, print to console

--- pages/allJournalEntries.tsx
+++ pages/allJournalEntries.tsx
@@ -1,11 +1,21 @@
-import * as React from 'react'
+import * as React from 'react';
+import { useQuery } from '@apollo/client';
+import { JOURNAL_ENTRIES_QUERY } from '../queries/queries';
 
 const AllJournalEntries: React.FC = () => {
-    return (
-        <main>
-          <p>All journal entries page</p>  
-        </main>
-    )
+  const { loading, error, data } = useQuery(JOURNAL_ENTRIES_QUERY);
+
+  let display: [];
+  if(!loading) {
+    display = data
+    console.log(display)
+  }
+  return (
+      <main>
+        {loading && <div>Loading journal entries..</div>}
+        {error && <div>{error}</div>}
+      </main>
+  )
 }
 
 export default AllJournalEntries;
