18bfb64989b2897cf5dc0367febf1308416c288d
Lucas Merchant <lbmerchant93@gmail.com>
2021-05-11 20:23:36 -0600

    Connect form to state

---
+++ interfaces/journalEntryForm.tsx
@@ -0,0 +1,10 @@
+export interface IJournalEntryData {
+    date: string;
+    prenatalVitamins: boolean;
+    probiotics: boolean;
+    waterIntake: number;
+    proteinIntake: number;
+    exercise: number;
+    kegels: number;
+    garlandPose: number;
+}
--- pages/newJournalEntry.tsx
+++ pages/newJournalEntry.tsx
@@ -1,30 +1,61 @@
-import * as React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import styles from '../styles/Form.module.scss';
+import { IJournalEntryData } from '../interfaces/journalEntryForm';
 
 const NewJournalEntry: React.FC = () => {
+
+    let formObj : IJournalEntryData;
+
+    const [formData, setFormData] = useState(formObj = {
+        date: '',
+        prenatalVitamins: false,
+        probiotics: false,
+        waterIntake: 0,
+        proteinIntake: 0,
+        exercise: 0,
+        kegels: 0,
+        garlandPose: 0
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        let newInput: string | number | boolean;
+        let numberInputs = ['waterIntake', 'proteinIntake', 'exercise', 'kegels', 'garlandPose'];
+        let booleanInputs = ['prenatalVitamins', 'probiotics']
+        if (numberInputs.includes(e.currentTarget.name)) {
+            newInput = parseInt(e.currentTarget.value)
+        } else if (booleanInputs.includes(e.currentTarget.name) && e.currentTarget.value === "true") {
+            newInput = true
+        } else if (booleanInputs.includes(e.currentTarget.name) && e.currentTarget.value === "false") {
+            newInput = false
+        } else {
+            newInput = e.currentTarget.value
+        }
+        setFormData({...formData, [e.currentTarget.name]: newInput})
+    }
+
     return (
         <main>
             <form className={styles.form}>
                 <label>Date: </label>
-                <input type='date' />
-                <label>Water Intake: </label>
-                <input type='number' placeholder='0' min='0' className={styles.numberInput}/>
+                <input type='date' name='date' onChange={handleChange}/>
                 <label>Prenatal Vitamins: 
-                    <label>Yes<input type='radio' value='true'/></label>
-                    <label>No<input type='radio' value='false' /></label>
+                    <label>Yes<input name='prenatalVitamins' type='radio' value='true' onChange={handleChange}/></label>
+                    <label>No<input name='prenatalVitamins' type='radio' value='false' onChange={handleChange}/></label>
                 </label>
                 <label>Probiotics: 
-                    <label>Yes<input type='radio' value='true' /></label>
-                    <label>No<input type='radio' value='false' /></label>
+                    <label>Yes<input name='probiotics' type='radio' value='true' onChange={handleChange}/></label>
+                    <label>No<input name='probiotics' type='radio' value='false' onChange={handleChange}/></label>
                 </label>
+                <label>Water Intake: </label>
+                <input type='number' name='waterIntake' placeholder='0' min='0' className={styles.numberInput} onChange={handleChange}/>
                 <label>Protein Intake: </label>
-                <input type='number' placeholder='0' min='0' className={styles.numberInput}/>
+                <input type='number' name='proteinIntake' placeholder='0' min='0' className={styles.numberInput} onChange={handleChange}/>
                 <label>Exercise: </label>
-                <input type='number' placeholder='0' min='0' className={styles.numberInput}/>
+                <input type='number' name='exercise' placeholder='0' min='0' className={styles.numberInput} onChange={handleChange}/>
                 <label>Kegels: </label>
-                <input type='number' placeholder='0' min='0' className={styles.numberInput}/>
+                <input type='number' name='kegels' placeholder='0' min='0' className={styles.numberInput} onChange={handleChange}/>
                 <label>Garland Pose: </label>
-                <input type='number' placeholder='0' min='0' className={styles.numberInput}/>
+                <input type='number' name='garlandPose' placeholder='0' min='0' className={styles.numberInput} onChange={handleChange}/>
                 <input type='submit' />
             </form> 
         </main>
