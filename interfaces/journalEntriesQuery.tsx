2fa0237fe40c95765bf909ca4f6f2034cdbbc49b
Lucas Merchant <lbmerchant93@gmail.com>
2021-05-14 10:37:15 -0600

    Create interface for a single journal entry

---
+++ interfaces/journalEntriesQuery.tsx
@@ -0,0 +1,12 @@
+export interface IJournalEntriesQuery {
+    id: string;
+    date: string;
+    prenatalVitamins: boolean;
+    probiotics: boolean;
+    waterIntake: number;
+    proteinIntake: number;
+    exercise: number;
+    kegels: number;
+    garlandPose: number;
+    userId: string;
+}
